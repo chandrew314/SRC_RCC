6cd6b8422a29b2bb668efa9cee885d9a5565662c
chandrew314 <axc1013@case.edu>
2024-02-11 16:49:35 -0500

    adding in command line options

--- RCC.py
+++ RCC.py
@@ -1,4 +1,18 @@
-#import pandas as pd
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("path")
+
+args = parser.parse_args()
+group = parser.add_mutually_exclusive_group()
+
+parser.add_argument('race_cost_pperson', help = 'int; base cost of signing up one person for the race--not accounting for processing fees or sales tax')
+parser.add_argument('n_people', help = 'int; number of people (including drivers) signed up for the race')
+parser.add_argument('amount_allocated', help = 'int; the amount of money allocated for the race by USG')
+parser.add_argument('-dd', '--driver_discount', help = 'boolean; will there be a discount for drivers? True/False', action = 'store_true') # might be optional
+# parser.add_argument('')
 
 def ceildiv(a, b):
     return -(a // -b)
@@ -14,8 +28,7 @@ def runsignup_processing_fee(race_cost):
         rsu_processing_fee = (race_cost * 0.04) + 2
     return rsu_processing_fee
 
-def race_cost(race_cost_pperson, n_people, runsignup):
-    sales_tax = 5
+def race_cost(race_cost_pperson, n_people, runsignup, sales_tax):
     max_people_for_runsignup = 25
     if runsignup == True:
         j = ceildiv(n_people, max_people_for_runsignup)
@@ -37,9 +50,9 @@ def race_cost(race_cost_pperson, n_people, runsignup):
         race_cost = race_cost_pperson * n_people
     return race_cost
 
-def discount_cost_per_person(race_cost_pperson, n_people, amount_allocated, bool_driver_discount = False, driver_discount = 0, n_drivers = 0, runsignup = True, profit = False, profit_margin = 0):
-    cost_not_covered_by_funding = race_cost(race_cost_pperson, n_people, runsignup) - amount_allocated
-    print(race_cost(race_cost_pperson, n_people, runsignup))
+def discount_cost_per_person(race_cost_pperson, n_people, amount_allocated, bool_driver_discount = False, driver_discount = 0, n_drivers = 0, runsignup = True, profit = False, profit_margin = 0, sales_tax = 5):
+    cost_not_covered_by_funding = race_cost(race_cost_pperson, n_people, runsignup, sales_tax) - amount_allocated
+    print(race_cost(race_cost_pperson, n_people, runsignup, sales_tax))
     if profit == False:
         cost_not_covered_by_funding = cost_not_covered_by_funding
     else:
@@ -50,16 +63,11 @@ def discount_cost_per_person(race_cost_pperson, n_people, amount_allocated, bool
         adj_cost_pperson = (cost_pperson + 0.3) / 0.971
         return adj_cost_pperson, adj_cost_pperson
     else:
-        # cost_not_covered_by_funding = cost_1 * (n_people - n_drivers) + cost_2 * n_drivers = cost_1*n_people - cost_1*n_drivers + cost_2*n_drivers = cost_1*n_people + n_drivers*(cost_2 - cost_1) = cost_1*n_people + n_drivers*(-driver_discount)
-        # cost_pnondriver = (cost_not_covered_by_funding + n_drivers * driver_discount) / n_people
-        # cost_pdriver = cost_pnondriver - driver_discount
-        # adj_cost_pnondriver = (cost_pnondriver + 0.3) / 0.971
-        # adj_cost_pdriver = (cost_pdriver + 0.3) / 0.971
-        # return adj_cost_pnondriver, adj_cost_pdriver
         cost_pnondriver = (((cost_not_covered_by_funding + n_drivers * ((driver_discount))) / n_people) + 0.3) / 0.971
         cost_pdriver = cost_pnondriver - driver_discount
         return cost_pnondriver, cost_pdriver
 
-adj_cost_pnondriver, adj_cost_pdriver = discount_cost_per_person(race_cost_pperson = 35, n_people = 30, amount_allocated = 703.5, bool_driver_discount = True, driver_discount = 5, n_drivers = 6, runsignup = True, profit = True, profit_margin = 100)
+adj_cost_pnondriver, adj_cost_pdriver = discount_cost_per_person(race_cost_pperson = 35, n_people = 30, amount_allocated = 703.5, bool_driver_discount = True, driver_discount = 5, n_drivers = 7, runsignup = True, profit = True, profit_margin = 100)
 
 print(adj_cost_pnondriver, adj_cost_pdriver)
+
